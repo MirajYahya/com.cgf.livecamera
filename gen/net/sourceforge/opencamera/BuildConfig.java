9838bcfa2ea5496c8745331eff88dadadf171746
dbluejp <dbluejp@gmail.com>
2014-12-23 09:22:30 +0900

    base update. (from open camera)
    base update.
    http://opencamera.sourceforge.net/

--- gen/net/sourceforge/opencamera/BuildConfig.java
+++ gen/net/sourceforge/opencamera/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.cgf;
+package net.sourceforge.opencamera;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
---
+++ src/com/cgf/livecamera/ToastBoxer.java
@@ -0,0 +1,10 @@
+package com.cgf.livecamera;
+
+import android.widget.Toast;
+
+public class ToastBoxer {
+	public Toast toast = null;
+
+	ToastBoxer() {
+	}
+}
